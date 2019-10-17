c86d429864fb4256268db41b5b88f944ffd4eed5
ERŽEN <me2291@student.uni-lj.si>
2019-10-17 12:38:42 +0200

    Final version

--- src/main/java/si/fri/prpo/servleti/JdbcServlet.java
+++ src/main/java/si/fri/prpo/servleti/JdbcServlet.java
@@ -1,9 +1,11 @@
 package si.fri.prpo.servleti;
 
-import javax.security.auth.login.Configuration;
+import com.kumuluz.ee.configuration.utils.ConfigurationUtil;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/servlet")
@@ -14,8 +16,10 @@ public class JdbcServlet extends HttpServlet {
 
         System.out.println("Console output");
 
-        resp.getWriter().println("Website output");
-        //resp.getWriter().println("Ime aplikacije:" + ConfigurationUtil.getInstance().get("kumuluzee.name").orElse("N/A"));
+        resp.getWriter().println("Website output\n");
+        resp.getWriter().println("Ime aplikacije: " + ConfigurationUtil.getInstance().get("kumuluzee.name").orElse("N/A"));
+        resp.getWriter().println("Verzija: " + ConfigurationUtil.getInstance().get("kumuluzee.version").orElse("N/A"));
+        resp.getWriter().println("Ime izvajalnega okolja: " + ConfigurationUtil.getInstance().get("kumuluzee.env.name").orElse("N/A"));
     }
 }
 
